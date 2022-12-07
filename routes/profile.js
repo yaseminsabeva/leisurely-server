419e1eb774a27f2586a0ddb2936e826655863f41
Yasemin Sabeva <yaseminsabeva@yahoo.com>
2022-12-07 23:20:35 +0100

    fix patches

--- routes/profile.js
+++ routes/profile.js
@@ -16,17 +16,42 @@ router.patch(
   uploader.single("picture"),
   isAuthenticated,
   async (req, res, next) => {
+    const { name, username, email } = req.body;
+
+    if (email === "" || name === "" || username === "") {
+      res.status(400).json({ message: "Please fill in the required fields." });
+      return;
+    }
+
+    const regexUsername = /^[a-z0-9_.]+$/;
+
+    if (!regexUsername.test(username)) {
+      return res.status(400).json({
+        message:
+          "Usernames can only use letters, numbers, underscores, and periods.",
+      });
+    }
+
     try {
-      //const user = await User.findById(req.payload.id).select("-password");
-      //res.status(200).json(user);
+      const foundUser = await User.findOne({ $or: [{ email }, { username }] });
       const { id } = req.payload;
+      console.log(id, foundUser.id);
+      if (foundUser && id !== foundUser.id) {
+        res
+          .status(400)
+          .json({ message: "Email or username are already in use." });
+        return;
+      }
       let picture = req.file?.path;
       const data = { ...req.body, picture };
       res
         .status(200)
         .json(await User.findByIdAndUpdate(id, data, { new: true }));
     } catch (error) {
-      next(error);
+      if (error instanceof mongoose.Error.ValidationError) {
+        return res.status(400).json({ message: error.message });
+      }
+      res.status(500).json({ message: "Something went terribly wrong." });
     }
   }
 );
