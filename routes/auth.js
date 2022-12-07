07bce4034033c59ce4d7a19521dadd73f051c07a
jeanne <jeanne.lefevre@hotmail.fr>
2022-12-07 10:34:12 +0100

    attendee profil route

--- routes/auth.js
+++ routes/auth.js
@@ -122,61 +122,61 @@ router.post("/signin", async (req, res, next) => {
 
 ////////////// PROFILE PAGE ///////////////////////
 
-router.get("/me", isAuthenticated, async (req, res, next) => {
-  //console.log("req payload", req.payload);
-  const user = await User.findById(req.payload.id).select("-password");
-  res.status(200).json(user);
-});
+// router.get("/me", isAuthenticated, async (req, res, next) => {
+//   //console.log("req payload", req.payload);
+//   const user = await User.findById(req.payload.id).select("-password");
+//   res.status(200).json(user);
+// });
 
-router.patch(
-  "/me",
-  uploader.single("picture"),
-  isAuthenticated,
-  async (req, res, next) => {
-    try {
-      //const user = await User.findById(req.payload.id).select("-password");
-      //res.status(200).json(user);
-      const { id } = req.payload;
-      let picture = req.file?.path;
-      const data = { ...req.body, picture };
-      res
-        .status(200)
-        .json(await User.findByIdAndUpdate(id, data, { new: true }));
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+// router.patch(
+//   "/me",
+//   uploader.single("picture"),
+//   isAuthenticated,
+//   async (req, res, next) => {
+//     try {
+//       //const user = await User.findById(req.payload.id).select("-password");
+//       //res.status(200).json(user);
+//       const { id } = req.payload;
+//       let picture = req.file?.path;
+//       const data = { ...req.body, picture };
+//       res
+//         .status(200)
+//         .json(await User.findByIdAndUpdate(id, data, { new: true }));
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
 
-router.delete("/me", isAuthenticated, async (req, res, next) => {
-  try {
-    const { id } = req.payload;
-    //console.log(id);
-    res.status(200).json(await User.findByIdAndDelete(id));
-  } catch (error) {
-    next(error);
-  }
-});
+// router.delete("/me", isAuthenticated, async (req, res, next) => {
+//   try {
+//     const { id } = req.payload;
+//     //console.log(id);
+//     res.status(200).json(await User.findByIdAndDelete(id));
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
-router.get("/me/hosted-events", isAuthenticated, async (req, res, next) => {
-  try {
-    const { id } = req.payload;
-    console.log(id);
-    res.status(200).json(await Event.find({ host: id }));
-  } catch (error) {
-    next(error);
-  }
-});
+// router.get("/me/hosted-events", isAuthenticated, async (req, res, next) => {
+//   try {
+//     const { id } = req.payload;
+//     console.log(id);
+//     res.status(200).json(await Event.find({ host: id }));
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
-router.get("/me/subscrided-events", isAuthenticated, async (req, res, next) => {
-  try {
-    const { id } = req.payload;
-    console.log(id);
-    res.status(200).json(await Event.find({ attendees: { $in: [id] } }));
-  } catch (error) {
-    next(error);
-  }
-});
+// router.get("/me/subscrided-events", isAuthenticated, async (req, res, next) => {
+//   try {
+//     const { id } = req.payload;
+//     console.log(id);
+//     res.status(200).json(await Event.find({ attendees: { $in: [id] } }));
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 ////////////// PROFILE PAGE ///////////////////////
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const User = require("../models/User.model");
+const Event = require("../models/Event.model");
 
-router.get("/:name", async (req, res, next) => {
-  const oneAttendee = await User.findOne({ name: req.params.name });
-  console.log(oneAttendee);
+router.get("/:username", async (req, res, next) => {
+  const { username } = req.params;
+  const oneAttendee = await User.findOne({ username });
+  const events = await Event.find({ host: oneAttendee._id });
+  oneAttendee._doc.events = events;
   res.status(200).json(oneAttendee);
 });
 
