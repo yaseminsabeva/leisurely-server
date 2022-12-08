c602e682c1a5524986647063d8af1efad9f3ec5c
Yasemin Sabeva <yaseminsabeva@yahoo.com>
2022-12-08 10:08:32 +0100

    add readme & rm commented code

---
+++ README.md
@@ -0,0 +1,27 @@
+# Leisurely 🎨🏂⚽️🛼💃🧶
+
+An events site made during week 9 of Ironhack's Web Development bootcamp as the third and final project. Users can browse for events, see who is attending, or sign up and add events themselves.
+
+#### In collaboration with:
+
+- [Jeanne](https://github.com/jannnitto/)
+- [Shruti](https://github.com/Sshaker2/)
+- [Yasemin](https://github.com/yaseminsabeva/)
+
+## About Implementation
+
+- A document based database hosted on MongoDB Atlas.
+- JWT for authentication.
+- Netlify for Frontend deployment.
+- Render for Backend deployment.
+
+## What could be next?
+
+- Responsive design and other design improvements.
+- Messaging, group chats.
+- Adding events to your favorites.
+- Address lookup, geolocalisation, geosearch.
+
+### [Link to Frontend Repo](https://github.com/yaseminsabeva/leisurely-client)
+
+### [Demo](https://leisurelyy.netlify.app)
--- routes/auth.js
+++ routes/auth.js
@@ -8,11 +8,7 @@ const Event = require("../models/Event.model");
 const User = require("../models/User.model");
 const saltRounds = 10;
 
-/**
- *
- * * All the routes are prefixed with `/api/auth`
- *
- */
+/** All the routes are prefixed with `/api/auth` */
 
 router.post("/signup", uploader.single("picture"), async (req, res, next) => {
   const { name, username, email, password, description } = req.body;
@@ -108,64 +104,5 @@ router.post("/signin", async (req, res, next) => {
     res.status(500).json({ message: "Something went terribly wrong." });
   }
 });
-
-////////////// PROFILE PAGE ///////////////////////
-
-// router.get("/me", isAuthenticated, async (req, res, next) => {
-//   //console.log("req payload", req.payload);
-//   const user = await User.findById(req.payload.id).select("-password");
-//   res.status(200).json(user);
-// });
-
-// router.patch(
-//   "/me",
-//   uploader.single("picture"),
-//   isAuthenticated,
-//   async (req, res, next) => {
-//     try {
-//       //const user = await User.findById(req.payload.id).select("-password");
-//       //res.status(200).json(user);
-//       const { id } = req.payload;
-//       let picture = req.file?.path;
-//       const data = { ...req.body, picture };
-//       res
-//         .status(200)
-//         .json(await User.findByIdAndUpdate(id, data, { new: true }));
-//     } catch (error) {
-//       next(error);
-//     }
-//   }
-// );
-
-// router.delete("/me", isAuthenticated, async (req, res, next) => {
-//   try {
-//     const { id } = req.payload;
-//     //console.log(id);
-//     res.status(200).json(await User.findByIdAndDelete(id));
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// router.get("/me/hosted-events", isAuthenticated, async (req, res, next) => {
-//   try {
-//     const { id } = req.payload;
-//     console.log(id);
-//     res.status(200).json(await Event.find({ host: id }));
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// router.get("/me/subscrided-events", isAuthenticated, async (req, res, next) => {
-//   try {
-//     const { id } = req.payload;
-//     console.log(id);
-//     res.status(200).json(await Event.find({ attendees: { $in: [id] } }));
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-////////////// PROFILE PAGE ///////////////////////
 
 module.exports = router;
