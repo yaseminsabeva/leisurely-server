0fdbfa9ed2367dc6850c9893ad646835a490ffac
jeanne <jeanne.lefevre@hotmail.fr>
2022-12-08 13:58:42 +0100

    attendees's profile fixed

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,11 @@ router.get("/:username", async (req, res, next) => {
   const { username } = req.params;
   const oneAttendee = await User.findOne({ username });
   const events = await Event.find({ host: oneAttendee._id });
+  const attendingEvent = await Event.find({
+    attendees: oneAttendee._id,
+  });
   oneAttendee._doc.events = events;
+  oneAttendee._doc.attendingEvent = attendingEvent;
   res.status(200).json(oneAttendee);
 });
 
